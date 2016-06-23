efd89b08fe91aa988bb59fa396eadf00ad760768
Dmitri Maximovich <maxim@maximdim.com>
2016-06-23 16:16:24 -0400

    add caching functionality

--- src/main/java/com/maximdim/getproxy/GetProxyMain.java
+++ src/main/java/com/maximdim/getproxy/GetProxyMain.java
@@ -1,14 +1,20 @@
 package com.maximdim.getproxy;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.http.HttpEntity;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -18,11 +24,17 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class GetProxyMain {
+	static final File DIR = new File(System.getProperty("user.home"), ".getproxy");
 	
 	public static void main(String[] args) throws Exception {
 		if (args.length != 1) {
 			System.err.println("Usage: "+GetProxyMain.class.getSimpleName()+" <targetUrl>");
 			System.exit(1);
+		}
+		
+		if (!DIR.exists()) {
+			DIR.mkdir();
+			System.out.println(DIR.getAbsolutePath()+" created");
 		}
 		
         Server server = new Server(9999);
@@ -47,6 +59,13 @@ public class GetProxyMain {
 				return;
 			}
 			System.out.println(target);
+			
+			File file = new File(DIR, DigestUtils.sha1Hex(target));
+			if (readFromFile(file, response)) {
+				System.out.println("Served cached version: "+file.getAbsolutePath());
+				return;
+			}
+			
 			HttpGet get = new HttpGet(this.base+target);
 			
 			Collections.list(request.getHeaderNames())
@@ -56,15 +75,43 @@ public class GetProxyMain {
 				});
 			
 			try(CloseableHttpResponse resp = httpclient.execute(get)) {
-				Arrays.stream(resp.getAllHeaders())
-					.forEach(h -> {
-						response.setHeader(h.getName(), h.getValue());
-					});
-				HttpEntity entity = resp.getEntity();
-				entity.writeTo(response.getOutputStream());
+				writeToFile(file, resp);
 			}
+			System.out.println("Saved to cache: "+file.getAbsolutePath());
+			readFromFile(file, response);
 		}
 	}
+
+	static void writeToFile(File f, CloseableHttpResponse resp) throws IOException {
+		try(BufferedOutputStream os = new BufferedOutputStream(new FileOutputStream(f))) {
+			copy(resp.getEntity().getContent(), os);
+			os.flush();
+		}
+	}
+	
+	static boolean readFromFile(File f, HttpServletResponse response) throws IOException {
+		if (!f.exists()) {
+			return false;
+		}
+		try(BufferedInputStream is = new BufferedInputStream(new FileInputStream(f))) {
+			copy(is, response.getOutputStream());
+		}
+		return true;
+	}
+	
+	static int copy(InputStream in, OutputStream out) throws IOException {
+		int byteCount = 0;
+		byte[] buffer = new byte[4096];
+		int bytesRead = -1;
+		while ((bytesRead = in.read(buffer)) != -1) {
+			out.write(buffer, 0, bytesRead);
+			byteCount += bytesRead;
+		}
+		out.flush();
+		return byteCount;
+	}
+	
+	
 }
 
 
